9c802fac3907e8394e10ad768c01d95717e8d5cc
Bababooey - aka Douglas Gaddum <DG246670@falmouth.ac.uk>
2024-03-13 13:21:04 +0000

    completed basic hand behind scenes

--- Assets/Hand/handInit.cs
+++ Assets/Hand/handInit.cs
@@ -2,46 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Unity.VisualScripting;
+using static UnityEngine.UI.Image;
+using UnityEngine.XR;
 
 public class handInit : MonoBehaviour
-{
+{  
+    System.Random rand = new System.Random();
     // Start is called before the first frame update
     public ScriptableObject[] startingHand;
-    public Dictionary<int?, ScriptableObject> Deck = new Dictionary<int, ScriptableObject>();
+    public Dictionary<string, ScriptableObject> Deck = new Dictionary<string , ScriptableObject>();
     void Awake()
     {
        for(int i = 0; i < startingHand.Length; i++)
         {
-            Deck.Add(i, startingHand[i]);
+            Deck.Add(startingHand[i].name, startingHand[i]);
         }
       
 
     }
 
 
+ public static KeyValuePair<string, ScriptableObject>[] drawCards(Dictionary<string, ScriptableObject> gamer, int amount)
+            {
+        KeyValuePair<string, ScriptableObject>[] drawnCards = new KeyValuePair<string, ScriptableObject>[amount];
+        for (int i = 0; i < gamer.Count; i++)
+        {
+            var drawnCard = gamer.ElementAt(0);
+            drawnCards[i] = drawnCard;
+            gamer.Remove(gamer.ElementAt(0).Key);
+        }
+      return drawnCards;
+ }
 
-    public void groupCardsByType(Dictionary<int?, ScriptableObject> deck, int groupType)
+ 
+    public Dictionary<string, ScriptableObject> groupCardsByType(Dictionary<string, ScriptableObject> deck, int? groupType)
     {
-
         switch(groupType)
         {
             default:
-
-
+                //WHO CAAAAAAAAAAAAREEEEEEEES
+              shuffleDeck(deck);
                 break;
-
-
         }
-
-
-
-
-
+        return deck;
     }
 
 
 
-
+    private Dictionary<string, ScriptableObject> shuffleDeck(Dictionary<string, ScriptableObject> deck)
+    {
+        deck = deck.OrderBy(x => rand.Next()).ToDictionary(item => item.Key, item => item.Value);
+        return deck;
+    }
 
 
 
